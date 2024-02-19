09006dd028927275cab0bee507415a81ad358df0
nikthe883 <popmihaylov@gmail.com>
2024-02-19 20:01:25 +0200

    solved

---
+++ Javascript/JS Syntax Fundamentals - Exercises/04.js
@@ -0,0 +1,11 @@
+function solve(num1, num2){
+    let result = []
+    let sum = 0
+    for(let i=num1; i <= num2; i++){
+        result += ` ${i}`
+        sum += i
+    }
+    console.log(result)
+    console.log(`Sum: ${sum}`)
+}
+solve(50, 60)

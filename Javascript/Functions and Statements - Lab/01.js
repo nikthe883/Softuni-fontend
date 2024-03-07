4f230cf55eb33053600a8c321b533e34290676a5
nikthe883 <popmihaylov@gmail.com>
2024-03-07 19:20:57 +0200

    update

--- Javascript/Functions and Statements - Lab/01.js
+++ Javascript/Functions and Statements - Lab/01.js
@@ -1,5 +1,6 @@
 function solve(grade){
     if (grade < 3.00){
+        
         console.log(`Fail (2)`)
     }else if(grade >= 3.00 && grade < 3.50){
         console.log(`Poor (${grade.toFixed(2)})`)

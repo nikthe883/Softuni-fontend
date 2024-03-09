417ede8e3e746c3797ebc63080cd659954da08d6
nikthe883 <popmihaylov@gmail.com>
2024-03-09 18:25:42 +0200

    update

---
+++ Javascript/Functions and Statements - Exercises/06.js
@@ -0,0 +1,27 @@
+function solve(pass){
+    function onlyLettersAndNumbers(pass) {
+        return Boolean(pass.match(/^[A-Za-z0-9]*$/));
+      }
+    function numbersBetweenSixAndTen(pass){
+        return Boolean(pass.length >= 6 && pass.length <= 10)
+    }
+    function AtLeastTwoDigits(pass){
+        return Boolean((pass.match(/\d/g) || []).length >= 2)
+    }
+ 
+    if (onlyLettersAndNumbers(pass) && numbersBetweenSixAndTen(pass) && AtLeastTwoDigits(pass)){
+        console.log('Password is valid')
+    }
+    if (!onlyLettersAndNumbers(pass)){
+        console.log("Password must consist only of letters and digits")
+    }
+    if (!numbersBetweenSixAndTen(pass)){
+
+        console.log('Password must be between 6 and 10 characters')
+    }
+    if (!AtLeastTwoDigits(pass)){
+        console.log("Password must have at least 2 digits")
+    }
+}
+
+solve('MyPass123')

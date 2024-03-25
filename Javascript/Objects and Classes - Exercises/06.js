d269b09126cad15f5ee44fe19ff0615fcc354ad6
nikthe883 <popmihaylov@gmail.com>
2024-03-25 19:25:27 +0200

    solved

---
+++ Javascript/Objects and Classes - Exercises/06.js
@@ -0,0 +1,20 @@
+function solve(input){
+    let [words, ...stringsToCheck] = input
+    const wordsObj = []
+    words.split(" ").map((word) => {
+        wordsObj.push({
+            word: word,
+            counter: 0,
+        });
+    })
+
+    wordsObj.map((obj) => {
+        for (let string of stringsToCheck) {
+            if (obj.word === string) obj.counter++;
+        }
+    });
+
+    wordsObj.sort((a, b) => b.counter - a.counter).map((obj) => {
+        console.log(`${obj.word} - ${obj.counter}`)
+    })
+}

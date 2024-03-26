519b45d400061597ed72be022c943608c4310c8e
nikthe883 <popmihaylov@gmail.com>
2024-03-26 18:53:50 +0200

    solved

--- Javascript/DOM Introduction - Lab/01. Edit Element_Ресурси/06.Sum Table/SumTable.js
+++ Javascript/DOM Introduction - Lab/01. Edit Element_Ресурси/06.Sum Table/SumTable.js
@@ -1,3 +1,14 @@
 function sumTable() {
+    let  textToExtract = Array.from(document.getElementsByTagName("td"));
+    let sum = 0;
+    for (let i = 1; i < textToExtract.length; i++)
+    
+        if (!isNaN(textToExtract[i].textContent)) {
+            
+            
+        sum += Number(textToExtract[i].textContent);
+        }
 
+    document.getElementById("sum").textContent = sum;
+    
 }
--- Javascript/DOM Introduction - Lab/01. Edit Element_Ресурси/07.Extract Parenthesis/ExtractParenthesis.js
+++ Javascript/DOM Introduction - Lab/01. Edit Element_Ресурси/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,6 @@
 function extract(content) {
-
+    let contents = document.getElementById("content").textContent
+    const regExp = /(?<=\()[^)]+(?=\))/g;
+    const matches = [...contents.match(regExp)];
+    return matches
 }
